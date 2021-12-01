924085aa34c74d36ef7b2b814f360ab692cc7a6c
Darren Clarke <darren@redaranj.com>
2021-12-01 13:33:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-const isProd = process.env.NODE_ENV === "production";
-const prefix = "/secureux.design";
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
   reactStrictMode: true,
-  basePath: isProd ? prefix : "",
-  assetPrefix: isProd ? prefix : "",
   trailingSlash: true,
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
 });
