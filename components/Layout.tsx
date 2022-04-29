08c2358dc558de1644540955593652a055fa37fa
Darren Clarke <darren@redaranj.com>
2022-04-29 10:01:20 +0000

    Remove console.log call

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,7 +11,6 @@ export const Layout: FC = ({ children }) => {
   const router = useRouter();
   const currentPhase = router.asPath.split("/")[1];
   const chapter = router.asPath.split("/")[2];
-  console.log({chapter})
   const {
     colors: { red, mintGreen, mustardYellow, lavender, lightBlue },
   } = useAppContext();
