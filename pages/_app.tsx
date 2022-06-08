3cd9e64b534fb471cc4d3647985b34d72f7597f3
Darren Clarke <darren@redaranj.com>
2022-06-08 18:41:02 +0200

    Add Twitter and Facebook cards

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,33 @@ const UXSecurityWeb = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <link rel="icon" type="image/png" href={Favicon.src} />
+
+      <meta name="twitter:site" content="@open_archive" />
+      <meta name="twitter:title" content="Human Rights Centered Design" />
+      <meta
+        name="twitter:description"
+        content="An online, open source curriculum focusing on how to center human rights throughout the research, design, and development processes."
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:image"
+        content="https://www.humanrightscentered.design/_next/static/media/uxs-icon-1.48c8c236.svg"
+      />
+
+      <meta property="og:title" content="Human Rights Centered Design" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://www.humanrightscentered.design"
+      />
+      <meta
+        name="og:description"
+        content="An online, open source curriculum focusing on how to center human rights throughout the research, design, and development processes."
+      />
+      <meta
+        property="og:image"
+        content="https://www.humanrightscentered.design/_next/static/media/uxs-icon-1.48c8c236.svg"
+      />
     </Head>
     <AppProvider>
       <CssBaseline />
